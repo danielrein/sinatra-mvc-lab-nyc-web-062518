562376ee0a19b0c3636132f30308e1524c6dc070
Daniel Ran <danielr1735@gmail.com>
2018-07-17 17:34:20 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+
+  attr_reader :text
+
+  def initizlize(text)
+    @text = text
+  end
+
+  def piglatinize(word)
+    while !"aeiou".include?(word.chr)
+      word = word + word.slice!(0)
+    end
+  end
+
+
+
+
+end
